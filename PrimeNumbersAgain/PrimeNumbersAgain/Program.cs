15a2dc2d423f2ee3d3ecbce8ef68dee3a27be202
Michael Tanczos <mike@logic-gate.com>
2024-09-17 08:17:31 -0400

    feat: Upgrade target framework and refactor code
    Upgraded the target framework from .NET 7.0 to .NET 8.0 in the project file. Refactored the main program by removing unnecessary nested classes, introducing a constant for maximum allowable time, and simplifying the structure of methods for better readability and maintainability.

--- PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
+++ PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
@@ -1,81 +1,83 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace PrimeNumbersAgain
+namespace PrimeNumbersAgain;
+
+class Program
 {
-    class Program
+    private const int MaxAllowableTime = 3;
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int n, prime;
+        Stopwatch timer = new Stopwatch();
+
+        PrintBanner();
+        n = GetNumber();
+
+        timer.Start();
+        prime = FindNthPrime(n);
+        timer.Stop();
+        
+        
+        Console.WriteLine($"\nToo easy.. {prime} is the nth prime when n is {n}. I found that answer in {timer.Elapsed.Seconds} seconds.");
+
+        EvaluatePassingTime(timer.Elapsed.Seconds);
+    }
+
+    static int FindNthPrime(int n)
+    {
+        return 0;
+    }
+
+    static int GetNumber()
+    {
+        int n = 0;
+        while (true)
         {
-            int n, prime;
-            Stopwatch timer = new Stopwatch();
-
-            PrintBanner();
-            n = GetNumber();
-
-            timer.Start();
-            prime = FindNthPrime(n);
-            timer.Stop();
+            Console.Write("Which nth prime should I find?: ");
             
-            
-            Console.WriteLine($"\nToo easy.. {prime} is the nth prime when n is {n}. I found that answer in {timer.Elapsed.Seconds} seconds.");
-
-            EvaluatePassingTime(timer.Elapsed.Seconds);
-        }
-
-        static int FindNthPrime(int n)
-        {
-            return 0;
-        }
-
-        static int GetNumber()
-        {
-            int n = 0;
-            while (true)
+            string num = Console.ReadLine();
+            if (Int32.TryParse(num, out n))
             {
-                Console.Write("Which nth prime should I find?: ");
-                
-                string num = Console.ReadLine();
-                if (Int32.TryParse(num, out n))
-                {
-                    return n;
-                }
-
-                Console.WriteLine($"{num} is not a valid number.  Please try again.\n");
-            }
-        }
-
-        static void PrintBanner()
-        {
-            Console.WriteLine(".................................................");
-            Console.WriteLine(".#####...#####...######..##...##..######...####..");
-            Console.WriteLine(".##..##..##..##....##....###.###..##......##.....");
-            Console.WriteLine(".#####...#####.....##....##.#.##..####.....####..");
-            Console.WriteLine(".##......##..##....##....##...##..##..........##.");
-            Console.WriteLine(".##......##..##..######..##...##..######...####..");
-            Console.WriteLine(".................................................\n\n");
-            Console.WriteLine("Nth Prime Solver O-Matic Online..\nGuaranteed to find primes up to 2 million in under 30 seconds!\n\n");
-            
-        }
-
-        static void EvaluatePassingTime(int time)
-        {
-            Console.WriteLine("\n");
-            Console.Write("Time Check: ");
-
-            if (time <= 10)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Pass");
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Fail");
+                return n;
             }
 
-            Console.ForegroundColor = ConsoleColor.Gray;
-            
+            Console.WriteLine($"{num} is not a valid number.  Please try again.\n");
         }
     }
+
+    static void PrintBanner()
+    {
+        Console.WriteLine(".................................................");
+        Console.WriteLine(".#####...#####...######..##...##..######...####..");
+        Console.WriteLine(".##..##..##..##....##....###.###..##......##.....");
+        Console.WriteLine(".#####...#####.....##....##.#.##..####.....####..");
+        Console.WriteLine(".##......##..##....##....##...##..##..........##.");
+        Console.WriteLine(".##......##..##..######..##...##..######...####..");
+        Console.WriteLine(".................................................\n\n");
+        Console.WriteLine($"Nth Prime Solver O-Matic Online..\nGuaranteed to find primes up to 2 million in under {MaxAllowableTime} seconds!\n\n");
+        
+    }
+
+    static void EvaluatePassingTime(int time)
+    {
+        Console.WriteLine("\n");
+        Console.Write("Time Check: ");
+
+        if (time <= MaxAllowableTime)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Pass");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Fail");
+        }
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+        
+    }
 }
+
